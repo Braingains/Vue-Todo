df311a3c2bd0185d8b19a8b317b1f40603e8c4c2
Ewan Brain <ewan.brain@gmail.com>
2021-01-11 18:07:40 +0000

    todo array created

--- src/app.js
+++ src/app.js
@@ -3,8 +3,9 @@ import Vue from 'vue';
 document.addEventListener('DOMContentLoaded', () => {
     new Vue({
         el: '#app',
+        data: {
+            todo: ['Wake up', 'Brush Teeth', 'Go to class', 'Do Homework', 'Work Out'],
 
-
-
+        },
     });
 });
